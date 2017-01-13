47a4f850164847b3a645e6260a5e39ac6c3135bd
Andreas Pardeike <andreas@pardeike.net>
2017-01-13 22:32:36 +0100

    initial checkin

---
+++ Source/Main.cs
@@ -0,0 +1,40 @@
+﻿using Verse;
+using Harmony;
+using UnityEngine;
+using System;
+
+namespace CameraPlus
+{
+	[StaticConstructorOnStartup]
+	class Main
+	{
+		static Main()
+		{
+			var harmony = HarmonyInstance.Create("net.pardeike.rimworld.mod.camera+");
+			harmony.PatchAll(typeof(Main).Module);
+		}
+	}
+
+	[HarmonyPatch(typeof(CameraDriver))]
+	[HarmonyPatch("ApplyPositionToGameObject")]
+	class CameraDriverPatch
+	{
+		static void Postfix(CameraDriver instance)
+		{
+			var trv = Traverse.Create(instance);
+			var p_MyCamera = trv.Property("MyCamera").GetValue<Camera>();
+			var pos = p_MyCamera.transform.position;
+			var y = p_MyCamera.transform.position.y;
+			var o = p_MyCamera.orthographicSize;
+
+			var y2 = GenMath.LerpDouble(65, 15, 65, 32, y);
+			var o2 = GenMath.LerpDouble(60, 11, 60, 0.5f, o);
+			var dolly = GenMath.LerpDouble(65, 15, 1, 3, y);
+			dolly = 85 - dolly * dolly * dolly * dolly;
+
+			p_MyCamera.transform.position = new Vector3(pos.x, y2, pos.z);
+			p_MyCamera.orthographicSize = o2;
+			instance.config.dollyRateKeys = dolly;
+		}
+	}
+}
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CameraPlus")]
+[assembly: AssemblyDescription("You want more zoom and different paning?")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andreas Pardeike")]
+[assembly: AssemblyProduct("CameraPlus")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b0d46bb5-08ff-4e04-9efd-3e32e38a9cc2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
