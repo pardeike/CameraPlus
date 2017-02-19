6aaeaf8db1a717a78b3a03d6f0785e961d734d61
Andreas Pardeike <andreas@pardeike.net>
2017-02-19 21:57:20 +0100

    v1.0.1 fixes bug with labels not showing

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 A RimWorld mod to improve the zoom factor for the build-in camera.
 
 Powered by [Harmony](https://github.com/pardeike/Harmony)  
-The runtime patch library for .NET
+The runtime patch library for Unity
 
 ![Harmony](https://s24.postimg.org/58bl1rz39/logo.png)
--- Source/Main.cs
+++ Source/Main.cs
@@ -2,6 +2,10 @@
 using Harmony;
 using UnityEngine;
 using System.Reflection;
+using Harmony.ILCopying;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace CameraPlus
 {
@@ -15,9 +19,107 @@ namespace CameraPlus
 		}
 	}
 
+	[HarmonyPatch(typeof(GenMapUI))]
+	[HarmonyPatch("DrawThingLabel")]
+	[HarmonyPatch(new Type[] { typeof(Vector2), typeof(string), typeof(Color) })]
+
+	static class GenMapUI_DrawThingLabel_Patch
+	{
+		static GameFont GetAdaptedGameFont()
+		{
+			var rootSize = Traverse.Create(Find.CameraDriver).Field("rootSize").GetValue<float>();
+			if (rootSize < 14f) return GameFont.Medium;
+			if (rootSize < 19f) return GameFont.Small;
+			return GameFont.Tiny;
+		}
+
+		class AdaptedGameFontReplacer : IILProcessor
+		{
+			bool firstInstruction = true;
+
+			public List<ILInstruction> Start(ILGenerator generator, MethodBase original)
+			{
+				return new List<ILInstruction>();
+			}
+
+			// we replace the first "GameFont.Tiny" with our "GetAdaptedGameFont()"
+			//
+			public List<ILInstruction> Process(ILInstruction instruction)
+			{
+				var result = new List<ILInstruction>();
+				if (firstInstruction && instruction.opcode == OpCodes.Ldc_I4_0)
+				{
+					var method = AccessTools.Method(typeof(GenMapUI_DrawThingLabel_Patch), "GetAdaptedGameFont");
+					var call = new ILInstruction(OpCodes.Call);
+					call.operand = method;
+					call.argument = method;
+					result.Add(call);
+				}
+				else
+					result.Add(instruction);
+
+				firstInstruction = false;
+				return result;
+			}
+
+			public List<ILInstruction> End(ILGenerator generator, MethodBase original)
+			{
+				return new List<ILInstruction>();
+			}
+		}
+
+		[HarmonyProcessors]
+		static HarmonyProcessor AdaptedGameFontReplacerPatch(MethodBase original)
+		{
+			var processor = new HarmonyProcessor();
+			processor.AddILProcessor(new AdaptedGameFontReplacer());
+			return processor;
+		}
+	}
+
+	[HarmonyPatch(typeof(CameraDriver))]
+	[HarmonyPatch("get_CurrentZoom")]
+	static class CameraDriver_get_CurrentZoom_Patch
+	{
+		class ZoomLerper : IILProcessor
+		{
+			public List<ILInstruction> Start(ILGenerator generator, MethodBase original)
+			{
+				return new List<ILInstruction>();
+			}
+
+			// Normal values: 12, 13.8, 42, 57
+			//
+			public List<ILInstruction> Process(ILInstruction instruction)
+			{
+				if (instruction.opcode == OpCodes.Ldc_R4)
+				{
+					var f = (float)instruction.operand;
+					f = GenMath.LerpDouble(12, 57, 30, 60, f);
+					instruction.operand = f;
+					instruction.argument = f;
+				}
+				return new List<ILInstruction>() { instruction };
+			}
+
+			public List<ILInstruction> End(ILGenerator generator, MethodBase original)
+			{
+				return new List<ILInstruction>();
+			}
+		}
+
+		[HarmonyProcessors]
+		static HarmonyProcessor LerpCurrentZoom(MethodBase original)
+		{
+			var processor = new HarmonyProcessor();
+			processor.AddILProcessor(new ZoomLerper());
+			return processor;
+		}
+	}
+
 	[HarmonyPatch(typeof(CameraDriver))]
 	[HarmonyPatch("ApplyPositionToGameObject")]
-	class CameraDriverPatch
+	static class CameraDriver_ApplyPositionToGameObject_Patch
 	{
 		static void Postfix(CameraDriver __instance)
 		{
