c6d1b9ef26ca1aa99a2d01674227e0b2ea9a39e9
Andreas Pardeike <andreas@pardeike.net>
2024-05-17 18:11:15 +0200

    wip

--- Source/Dialog_Colors.cs
+++ Source/Dialog_Colors.cs
@@ -8,10 +8,13 @@ namespace CameraPlus
 {
 	public class Dialog_Colors : Window
 	{
-		public override Vector2 InitialSize => new(950, 420);
+		public override Vector2 InitialSize => new(950, 640);
+		private Vector2 scrollPosition = Vector2.zero;
 
+		const float rowHeight = 30f;
+		const float rowSpacing = 10f;
 		static readonly Vector2 previewSize = new(100, 30);
-		static readonly Vector2 colorFieldSize = new(100, 30);
+		static readonly Vector2 colorFieldSize = new(100, rowHeight);
 		static readonly Color borderColor = Color.gray.ToTransparent(0.5f);
 
 		public Dialog_Colors()
@@ -20,10 +23,13 @@ namespace CameraPlus
 			draggable = true;
 		}
 
-		static void PrepareRow(Listing_Standard list, List<ConditionTag> tags, out Rect previewRect, out Rect cRect1, out Rect cRect2, out Rect cRect3, out Rect cRect4)
+		static void PrepareRow(Listing_Standard list, bool first, List<ConditionTag> tags, out Rect previewRect, out Rect cRect1, out Rect cRect2, out Rect cRect3, out Rect cRect4)
 		{
+			if (first == false)
+				list.Gap(rowSpacing);
+
 			var h = colorFieldSize.y;
-			var rect = list.GetRect(h + 10);
+			var rect = list.GetRect(h);
 
 			var labelRect = rect.LeftPartPixels(rect.width - 4 * (Dialog_ColorPicker.spacing + colorFieldSize.x) - Dialog_ColorPicker.spacing - previewSize.x);
 			previewRect = new Rect(labelRect.xMax + Dialog_ColorPicker.spacing, labelRect.y, previewSize.x, h).Rounded();
@@ -63,16 +69,16 @@ namespace CameraPlus
 		void DrawPreview(Rect previewRect, bool isSelected, Color outerColor, Color innerColor)
 		{
 			var rect = new Rect(previewRect.center.x + (isSelected ? 5 : -previewRect.height - 5), previewRect.y, previewRect.height, previewRect.height);
-			var material = Assets.previewMaterial;
-			material.SetColor("_OutlineColor", outerColor);
-			material.SetColor("_FillColor", innerColor);
-			material.SetFloat("_OutlineFactor", 0.35f);
-			GenUI.DrawTextureWithMaterial(rect.ContractedBy(4).Rounded(), Assets.dummyTexture, material);
+			GUI.color = outerColor;
+			GUI.DrawTexture(rect.ContractedBy(4).Rounded(), Assets.outerColonistTexture);
+			GUI.color = innerColor;
+			GUI.DrawTexture(rect.ContractedBy(4).Rounded(), Assets.innerColonistTexture);
+			GUI.color = Color.white;
 		}
 
-		void ColorEditorRow(Listing_Standard list, DotConfig dotConfig)
+		void ColorEditorRow(Listing_Standard list, bool first, DotConfig dotConfig)
 		{
-			PrepareRow(list, dotConfig.conditions, out var previewRect, out var cRect1, out var cRect2, out var cRect3, out var cRect4);
+			PrepareRow(list, first, dotConfig.conditions, out var previewRect, out var cRect1, out var cRect2, out var cRect3, out var cRect4);
 
 			DrawPreview(previewRect, false, dotConfig.lineColor, dotConfig.fillColor);
 			DrawPreview(previewRect, true, dotConfig.lineSelectedColor, dotConfig.fillSelectedColor);
@@ -87,21 +93,30 @@ namespace CameraPlus
 
 		public override void DoWindowContents(Rect inRect)
 		{
+			inRect.yMax -= 40; // close button footer
+
 			var list = new Listing_Standard();
 			list.Begin(inRect);
 
-			PrepareRow(list, null, out var previewRect, out var cRect1, out var cRect2, out var cRect3, out var cRect4);
-
+			PrepareRow(list, true, null, out var previewRect, out var cRect1, out var cRect2, out var cRect3, out var cRect4);
 			Widgets.DrawTextureFitted(previewRect, Assets.columnHeaderPreview, 1);
 			Widgets.DrawTextureFitted(cRect1.Union(cRect2), Assets.columnHeader, 1);
 			Widgets.DrawTextureFitted(cRect3.Union(cRect4), Assets.columnHeaderSelected, 1);
+			list.Gap(rowSpacing);
+			var outRect = list.GetRect(inRect.height - list.curY - rowSpacing - 30);
 
 			var dotConfigs = Find.World.GetComponent<CameraSettings>().dotConfigs;
-			foreach (var dotConfig in dotConfigs)
-			{
-				ColorEditorRow(list, dotConfig);
-				list.Gap(20);
-			}
+			var configCount = dotConfigs.Count;
+			var viewRect = new Rect(0, 0, inRect.width - 16, rowHeight * configCount + rowSpacing * (configCount - 1));
+			Widgets.BeginScrollView(outRect, ref scrollPosition, viewRect);
+			var innerList = new Listing_Standard();
+			innerList.Begin(viewRect);
+			for (var row = 0; row < configCount; ++row)
+				ColorEditorRow(innerList, row == 0, dotConfigs[row]);
+			innerList.End();
+			Widgets.EndScrollView();
+
+			list.Gap(rowSpacing);
 			if (list.ButtonText("New".TranslateSimple(), null, 0.2f))
 				dotConfigs.Add(new DotConfig());
 
